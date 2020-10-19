bf119dd5a0f92424f22584dc93461693aafeb1a2
minhtintang <minhtintang@gmail.com>
2020-10-19 21:23:12 +0700

    init web

---
+++ SaleLaptopSystem/SaleLaptopSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SaleLaptopSystem.Startup))]
+namespace SaleLaptopSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
