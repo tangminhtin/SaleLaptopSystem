f9a3780353c4b0950b21945a77a8817cd067698b
quanghien140311 <63026166+quanghien140311@users.noreply.github.com>
2020-10-28 08:31:56 +0700

    function Search

---
+++ SaleLaptopSystem/SaleLaptopSystem/Controllers/SearchController.cs
@@ -0,0 +1,91 @@
+﻿using SaleLaptopSystem.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaleLaptopSystem.Controllers
+{
+    public class SearchController : Controller
+    {
+        SaleLaptopSystemContext db = new SaleLaptopSystemContext();
+        // GET: Search
+        public ActionResult Index(String key)
+        {
+            return View(db.Products.Where(x => x.Name.Contains(key) || key == null).ToList());
+        }
+
+        // GET: Search/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Search/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Search/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Search/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Search/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Search/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Search/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
